1f3c7605a0509a4314785c869dc19af4f8198849
hitesh <hiteshchauhan.669@gmail.com>
2020-04-09 01:22:53 +0530

    fixes

--- src/server/index.js
+++ src/server/index.js
@@ -4,19 +4,19 @@ import path from 'path';
 
 const app = express();
 
-if (process.env !== 'production') {
-    import config from '../../config/webpack.dev';
-    import webpack from 'webpack';
-    const compiler = webpack(config);
-    const webpackDevMiddleware = require('webpack-dev-middleware')(
-        compiler,
-        config.devServer
-    )
-    const webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
+// if (process.env !== 'production') {
+//     import config from '../../config/webpack.dev';
+//     import webpack from 'webpack';
+//     const compiler = webpack(config);
+//     const webpackDevMiddleware = require('webpack-dev-middleware')(
+//         compiler,
+//         config.devServer
+//     )
+//     const webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
 
-    app.use(webpackDevMiddleware);
-    app.use(webpackHotMiddleware)
-}
+//     app.use(webpackDevMiddleware);
+//     app.use(webpackHotMiddleware)
+// }
 
 const staticMiddleware = express.static(path.resolve(__dirname, '../../dist'));
 app.use(staticMiddleware)
